a54daed57ac50774b827f0626a731add2c2f7788
Naoki-Nomi <n.naoki.116@gmail.com>
2021-04-09 08:54:51 +0900

    Initial Commit

---
+++ Samplequiz/ScoreViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ScoreViewController.swift
+//  Samplequiz
+//
+//  Created by Naoki Nomi on 2021/04/09.
+//
+
+import UIKit
+
+class ScoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
